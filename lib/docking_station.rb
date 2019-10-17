cf88ececee5f52a31a5298866fb31e743a57a755
Andy Jones <andytjones@icloud.com>
2019-10-17 20:56:53 +0100

    Step 11 done. User can dock a bike and get readout of bikes.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,19 @@
 require_relative 'bike'
 
 class DockingStation
+
+  attr_reader :docked_bikes
+  
+  def initialize
+    @docked_bikes = []
+  end
+  
+
   def release_bike
     Bike.new
+  end
+
+  def dock_bike(bike_name)
+    docked_bikes.push(bike_name)
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,9 +3,30 @@ require 'docking_station'
 describe DockingStation do
 
   it { is_expected.to respond_to :release_bike }
-  
+
   it 'releases working bikes' do
     bike = subject.release_bike
     expect(bike).to be_working
   end
+
+  it { is_expected.to respond_to :dock_bike }
+
+  it { is_expected.to respond_to :docked_bikes }
+
+  it "docked bikes" do
+    expect(subject.docked_bikes).to be_a(Array)
+  end
+
+  it "should accept a argument" do
+    station = DockingStation.new
+    expect { station.dock_bike( :argument ) }.to_not raise_error
+  end
+
+  it "does dock_bike return argument passed to it" do
+    station = DockingStation.new
+    bike = Bike.new
+    station.dock_bike(bike)
+    expect(station.docked_bikes).to include(bike)
+  end
+
 end
